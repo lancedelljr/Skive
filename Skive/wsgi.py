cc96c171f7c1884e57bfdc847dafb1fe34f44310
lancedelljr <lancedelljr@gmail.com>
2017-04-26 14:13:04 -0500

    Update wsgi.py

--- Skive/wsgi.py
+++ Skive/wsgi.py
@@ -2,7 +2,11 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
+# sets the settings environment variable to use the production settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Skive.settings.production")
 
+# web server gateway interface (wsgi)
 application = get_wsgi_application()
+
+# whitenoise allows the web app to serve its own staticfiles
 application = DjangoWhiteNoise(application)
